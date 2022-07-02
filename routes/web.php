3f25523f956ad0d8cf59c8feed79bdde558b5fe6
Ali Jalizavi <94685797+AliJalizavi@users.noreply.github.com>
2022-07-02 11:22:56 +0430

    route folder have been changed

--- routes/web.php
+++ routes/web.php
@@ -8,23 +8,18 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
-//home
+
+//home controler
+
 Route::get('/', function () {
     return view('home');
 })->name('home');
-//dashboard
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->name('dashboard');
 
-Route::get('/users/{user:username}/posts', [UserPostController::class, 'index'])->name('users.posts');
+//dashboard controler
 
-Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store']);
-
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'store']);
+//posts controler
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
@@ -33,3 +28,12 @@ Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.
 
 Route::post('/posts/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
 Route::delete('/posts/{post}/likes', [PostLikeController::class, 'destroy'])->name('posts.likes');
+Route::get('/users/{user:username}/posts', [UserPostController::class, 'index'])->name('users.posts');
+//log in logout controler
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
